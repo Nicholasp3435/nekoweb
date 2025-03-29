fc5896d32222e7cbfc1fec5e31b3e0e1c4678e61
Nicholas <nap44794@uga.edu>
2025-03-29 01:18:21 -0400

    refactor: apply glMatrix shortcut

--- coding_projects/3d_rendering/3d.js
+++ coding_projects/3d_rendering/3d.js
@@ -1,3 +1,5 @@
+const { vec3, vec4, mat4, quat } = glMatrix;
+
 class Scene {
     constructor(canvas) {
         this.canvas = canvas;
@@ -8,7 +10,19 @@ class Scene {
 
         this.frame = 0;
 
-        this.lightVec = glMatrix.vec3.fromValues(1, 0, 0);
+        this.lightVec = vec3.fromValues(1, 0, 0);
+
+        this.isMouseDragging = false;
+
+        this.canvas.addEventListener('mousedown', () => this.isMouseDragging = true);
+        this.canvas.addEventListener('mouseup', () => this.isMouseDragging = false);
+    
+        this.canvas.addEventListener('mousemove', e => {
+            if (!this.isMouseDragging) {
+                return;
+            }
+            console.log(e)
+        });
     }
 
     addModel(model) {
@@ -20,8 +34,8 @@ class Scene {
     }
 
     setLight(x, y, z) {
-        this.lightVec = glMatrix.vec3.fromValues(x, y, z);
-        glMatrix.vec3.normalize(this.lightVec, this.lightVec);
+        this.lightVec = vec3.fromValues(x, y, z);
+        vec3.normalize(this.lightVec, this.lightVec);
     }
 
     draw() {
@@ -53,19 +67,19 @@ class Model {
         this.transformedVectors;
         this.transformedFaces = faces;
         
-        this.scaleVec = glMatrix.vec3.fromValues(1, 1, 1);
+        this.scaleVec = vec3.fromValues(1, 1, 1);
 
-        this.center = glMatrix.vec3.create();
+        this.center = vec3.create();
         this.vectors.forEach(vector => {
-            glMatrix.vec3.add(this.center, this.center, vector);
+            vec3.add(this.center, this.center, vector);
         });
-        glMatrix.vec3.scale(this.center, this.center, 1 / this.vectors.length);
+        vec3.scale(this.center, this.center, 1 / this.vectors.length);
 
-        this.furthest = glMatrix.vec3.create();
+        this.furthest = vec3.create();
     
         this.vectors.forEach(vector => {
-            if (glMatrix.vec3.distance(vector, this.center) >= glMatrix.vec3.distance(this.furthest, this.center)) {
-                glMatrix.vec3.copy(this.furthest, vector);
+            if (vec3.distance(vector, this.center) >= vec3.distance(this.furthest, this.center)) {
+                vec3.copy(this.furthest, vector);
             }
         });
 
@@ -75,7 +89,7 @@ class Model {
             z: 0
         }
 
-        this.rotationQuat = glMatrix.quat.fromValues(
+        this.rotationQuat = quat.fromValues(
             this.rotation.x,
             this.rotation.y,
             this.rotation.z,
@@ -85,10 +99,10 @@ class Model {
         this.translation = {
             x: -this.center[0],
             y: -this.center[1],
-            z: glMatrix.vec3.length(this.furthest) * 1.5
+            z: vec3.length(this.furthest) * 1.5
         }
 
-        this.translationVec = glMatrix.vec3.fromValues(
+        this.translationVec = vec3.fromValues(
             this.translation.x,
             this.translation.y,
             this.translation.z
@@ -96,8 +110,8 @@ class Model {
 
         this.scale = 1;
 
-        this.transformationMat = glMatrix.mat4.create();
-        glMatrix.mat4.fromRotationTranslationScaleOrigin(
+        this.transformationMat = mat4.create();
+        mat4.fromRotationTranslationScaleOrigin(
             this.transformationMat,
             this.rotationQuat,
             this.translationVec,
@@ -105,8 +119,8 @@ class Model {
             this.center
         );
 
-        this.perspectiveMat = glMatrix.mat4.create();
-        this.perspectiveMat = glMatrix.mat4.perspective(
+        this.perspectiveMat = mat4.create();
+        this.perspectiveMat = mat4.perspective(
             this.perspectiveMat,
             .01,
             1,
@@ -121,7 +135,7 @@ class Model {
     }
 
     setRotation(x, y, z) {
-        glMatrix.quat.fromEuler(this.rotationQuat, x, y, z);
+        quat.fromEuler(this.rotationQuat, x, y, z);
         this.rotation.x = x;
         this.rotation.y = y;
         this.rotation.z = z;
@@ -136,7 +150,7 @@ class Model {
     }
 
     setTranslation(x, y, z) {
-        this.translationVec = glMatrix.vec3.fromValues(x, y, z);
+        this.translationVec = vec3.fromValues(x, y, z);
         this.translation.x = x;
         this.translation.y = y;
         this.translation.z = z;
@@ -151,12 +165,12 @@ class Model {
     }
 
     setScale(s) {
-        this.translationVec = glMatrix.vec3.fromValues(s, s, s);
+        this.translationVec = vec3.fromValues(s, s, s);
         this.scale = s;
     }
 
     setTransformation() {
-        glMatrix.mat4.fromRotationTranslationScaleOrigin(
+        mat4.fromRotationTranslationScaleOrigin(
             this.transformationMat,
             this.rotationQuat,
             this.translationVec,
@@ -166,15 +180,15 @@ class Model {
     }
 
     transformVector(vector) {
-        let transformedVector = glMatrix.vec4.clone(vector);
-        glMatrix.vec4.transformMat4(transformedVector, transformedVector, this.transformationMat);
+        let transformedVector = vec4.clone(vector);
+        vec4.transformMat4(transformedVector, transformedVector, this.transformationMat);
         return transformedVector;
     }
 
     perspectiveDivide(vector) {
-        glMatrix.vec4.transformMat4(vector, vector, this.perspectiveMat);
+        vec4.transformMat4(vector, vector, this.perspectiveMat);
         
-        let pers = glMatrix.vec4.fromValues(
+        let pers = vec4.fromValues(
             vector[0] / vector[3],
             vector[1] / vector[3],
             vector[2] / vector[3],
@@ -197,17 +211,17 @@ class Model {
     }
 
     zSortFaces() {
-        let normalMat = glMatrix.mat4.create();
-        glMatrix.mat4.invert(normalMat, this.transformationMat);
-        glMatrix.mat4.transpose(normalMat, normalMat);
+        let normalMat = mat4.create();
+        mat4.invert(normalMat, this.transformationMat);
+        mat4.transpose(normalMat, normalMat);
     
         this.transformedFaces = this.faces.map(face => {
             let transCenter = this.transformVector(face.center);
             transCenter = this.perspectiveDivide(transCenter);
     
-            let transNormal = glMatrix.vec4.create();
-            glMatrix.vec4.transformMat4(transNormal, face.normal, normalMat);
-            glMatrix.vec4.normalize(transNormal, transNormal);
+            let transNormal = vec4.create();
+            vec4.transformMat4(transNormal, face.normal, normalMat);
+            vec4.normalize(transNormal, transNormal);
     
             return {
                 vectors: face.vectors,
@@ -229,20 +243,20 @@ class Model {
     }
 
     drawShaded(scene, normal) {
-        let norm3 = glMatrix.vec3.fromValues(
+        let norm3 = vec3.fromValues(
             normal[0],
             normal[1],
             normal[2]
         );
 
-        glMatrix.vec3.normalize(norm3, norm3);
+        vec3.normalize(norm3, norm3);
 
-        let lightness = glMatrix.vec3.dot(norm3, scene.lightVec) * 100;
+        let lightness = vec3.dot(norm3, scene.lightVec) * 100;
 
         scene.ctx.fillStyle = `hsl(300, 100%, ${lightness}%)`;
         scene.ctx.strokeStyle = `hsl(300, 100%, ${lightness}%)`;
 
-        if (glMatrix.vec3.dot(norm3, glMatrix.vec3.fromValues(0, 0, 1)) < 0) {
+        if (vec3.dot(norm3, vec3.fromValues(0, 0, 1)) < 0) {
             scene.ctx.stroke();
             scene.ctx.fill();   
         }
@@ -250,7 +264,7 @@ class Model {
 
     flipNormals() {
         this.faces.forEach(face => {
-            face.normal = glMatrix.vec4.fromValues(
+            face.normal = vec4.fromValues(
                 -face.normal[0],
                 -face.normal[1],
                 -face.normal[2],
@@ -348,7 +362,7 @@ function read_obj(text) {
 function parse_vertices_faces([vertices, faces, normals]) {
 
     let parsed_vertices = vertices.map(vertex => {
-        return glMatrix.vec4.fromValues(
+        return vec4.fromValues(
             vertex[0],
             vertex[1],
             vertex[2],
@@ -375,25 +389,25 @@ function parse_vertices_faces([vertices, faces, normals]) {
                 parseInt(face[1].split('/')[0] - 1),
                 parseInt(face[2].split('/')[0] - 1),
             ],
-            normal: glMatrix.vec4.fromValues(
+            normal: vec4.fromValues(
                 normal[0],
                 normal[1],
                 normal[2],
                 1
             ),
-            center: glMatrix.vec4.fromValues(0, 0, 0, 1)
+            center: vec4.fromValues(0, 0, 0, 1)
         }
 
-        if (glMatrix.vec4.exactEquals(parsed_face.normal, glMatrix.vec4.fromValues(0, 0, 0, 1))) {
-            let ab = glMatrix.vec3.create();
-            glMatrix.vec3.subtract(ab, parsed_vertices[parsed_face.vectors[1]], parsed_vertices[parsed_face.vectors[0]]);
-            let ac = glMatrix.vec3.create();
-            glMatrix.vec3.subtract(ac, parsed_vertices[parsed_face.vectors[2]], parsed_vertices[parsed_face.vectors[0]]);
+        if (vec4.exactEquals(parsed_face.normal, vec4.fromValues(0, 0, 0, 1))) {
+            let ab = vec3.create();
+            vec3.subtract(ab, parsed_vertices[parsed_face.vectors[1]], parsed_vertices[parsed_face.vectors[0]]);
+            let ac = vec3.create();
+            vec3.subtract(ac, parsed_vertices[parsed_face.vectors[2]], parsed_vertices[parsed_face.vectors[0]]);
 
-            let normal = glMatrix.vec3.create();
-            glMatrix.vec3.cross(normal, ab, ac);
+            let normal = vec3.create();
+            vec3.cross(normal, ab, ac);
 
-            parsed_face.normal = glMatrix.vec4.fromValues(
+            parsed_face.normal = vec4.fromValues(
                 normal[0],
                 normal[1],
                 normal[2],
@@ -401,12 +415,12 @@ function parse_vertices_faces([vertices, faces, normals]) {
             )
         }
 
-        glMatrix.vec4.add(
+        vec4.add(
             parsed_face.center, 
             parsed_vertices[parsed_face.vectors[0]],
             parsed_vertices[parsed_face.vectors[1]]
         );
-        glMatrix.vec4.scaleAndAdd(
+        vec4.scaleAndAdd(
             parsed_face.center,
             parsed_face.center,
             parsed_vertices[parsed_face.vectors[2]],
@@ -469,7 +483,6 @@ async function init_scene() {
 
     mainScene.addModel(eevee);
     eevee.setRotation(0, 180, 0);
-    // eevee.flipNormals()
 
     mainScene.setLight(1, 0, -1);
 
@@ -488,49 +501,4 @@ function drawLoop() {
     requestAnimationFrame(drawLoop);
 }
 
-init_scene()
-
-const handleMovement = (e) => {
-    switch (e.key) {
-        case 'a':
-            eevee.addRotation(0, -1, 0);
-            break;
-        case 'd':
-            eevee.addRotation(0, 1, 0);
-            break;
-        case 'w':
-            eevee.addRotation(-1, 0, 0);
-            break;
-        case 's':
-            eevee.addRotation(1, 0, 0);
-            break;
-        case 'q':
-            eevee.addRotation(0, 0, -1);
-            break;
-        case 'e':
-            eevee.addRotation(0, 0, 1);
-            break;
-
-        case 'i':
-            eevee.addTranslation(0, .1, 0);
-            break;
-        case 'k':
-            eevee.addTranslation(0, -.1, 0);
-            break;
-        case 'j':
-            eevee.addTranslation(.1, 0, 0);
-            break;
-        case 'l':
-            eevee.addTranslation(-.1, 0, 0);
-            break;
-        case 'h':
-            eevee.addTranslation(0, 0, .1);
-            break;
-        case 'n':
-            eevee.addTranslation(0, 0, -.1);
-            break;
-
-    }
-};
-
-window.addEventListener('keydown', handleMovement);
+init_scene();
